f762b3b04eb5fdf8f3a2a3bb939c92e9544244c6
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2019-12-16 18:09:57 +0200

    created user and called mathod with wrong value, coz no such modifiers exist

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ package main.java;
 
 public class Application {
     public static void main(String[] args) {
-
+        User u = new User();
+        System.out.println(u.getName());
     }
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -3,4 +3,13 @@ package main.java;
 public class User {
     private int age;
     priavate String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
